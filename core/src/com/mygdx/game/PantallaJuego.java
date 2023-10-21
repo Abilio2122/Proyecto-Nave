69ba3b6497e9e4d0b0000c1c88cfb176c3aab5e0
sebastian019 <127500554+sebastian019@users.noreply.github.com>
2023-10-21 15:58:02 -0300

    Agregue unas cosas para el apartado audiovisual

--- core/src/com/mygdx/game/PantallaJuego.java
+++ core/src/com/mygdx/game/PantallaJuego.java
@@ -47,7 +47,7 @@ public class PantallaJuego implements Screen {
 		//inicializar assets; musica de fondo y efectos de sonido
 		explosionSound = Gdx.audio.newSound(Gdx.files.internal("explosion.ogg"));
 		explosionSound.setVolume(1,0.5f);
-		gameMusic = Gdx.audio.newMusic(Gdx.files.internal("piano-loops.wav")); //
+		gameMusic = Gdx.audio.newMusic(Gdx.files.internal("noMeConoce.wav")); //
 		
 		gameMusic.setLooping(true);
 		gameMusic.setVolume(0.5f);
